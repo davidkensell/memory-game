11a04a801c114b3e752338de786864cb85731b2d
davidkensell <d.kensell@pixelz.com>
2018-10-14 11:48:57 -0700

    Add game over Congrats modal

--- js/app.js
+++ js/app.js
@@ -199,7 +199,7 @@ function misMatch(){
   let mismatches = document.querySelectorAll('.open');
   mismatches[0].classList.add('mismatch');
   mismatches[1].classList.add('mismatch');
-  setTimeout(clearOpen, 1000);
+  setTimeout(clearOpen, 500);
 }
 
 // Clear opened cards css and from tracking array
@@ -238,7 +238,35 @@ function clearWatch(){
   timer.textContent = "";
 }
 
-// Game Over! Toggle modal when matched = 8
+// Game Over! Toggle modal when matched = 8. Based off https://www.w3schools.com/howto/howto_css_modals.asp
 function gameOver(){
+  const modal = document.getElementById('myModal');
+  const span = document.getElementsByClassName("close")[0];
 
+  const btn = document.getElementById("reBtn");
+  const score = document.querySelector('.score-panel');
+  const final = document.querySelector('.final');
+
+  let scoreguts = score.innerHTML;
+  final.insertAdjacentHTML('afterbegin', scoreguts);
+
+  modal.style.display = "block";
+
+  // When replay button clicked, close modal and restart
+  btn.onclick = function() {
+    modal.style.display = "none";
+    reStart();
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+      modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  }
 }
